5235eb6d68d4f8545db54e4fa574f9960b74f3df
MDaams <martijn.daams@kabisa.nl>
2019-03-19 13:54:05 +0100

    Implement assignment

---
+++ src/basic/toDo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class ToDo extends React.Component {
+  // Requirements:
+  // 1. User can add an todo item
+  // 2. User can see a list of added to do items
+  render() {
+    return (
+      <div>
+        <input />
+        <button>Add todo</button>
+      </div>
+    );
+  }
+}
