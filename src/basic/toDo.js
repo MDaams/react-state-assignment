d87334888198a0ff04a704e2b98d73756b601431
MDaams <martijn.daams@kabisa.nl>
2019-03-19 14:48:48 +0100

    Add bonus assignments

---
+++ src/basic/toDo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export class ToDo extends React.Component {
+  // Requirements:
+  // 1. User can add an todo item.
+  // 2. User can see a list of added to do items.
+  // Bonus requirements:
+  // 1. Button is disabled when input is empty.
+  // 2. Clear input when item has been added to the todo list.
+  render() {
+    return (
+      <div>
+        <input />
+        <button>Add todo</button>
+      </div>
+    );
+  }
+}
